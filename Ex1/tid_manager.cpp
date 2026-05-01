3dbc85f2b998c8a5ead689d9a483067b5e1ae688
Guy Prizat <guyprizat@Guys-MacBook-Pro.local>
2026-05-01 09:12:32 +0300

    Updated data structures - main thread vector with pointers and an ID deque, also debugging of a few functions such as context switch, sleep, init, spawn,terminate.

---
+++ Ex1/tid_manager.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <set>
+#include <sys/time.h>
+#include <stack>
+#include <stdlib.h>
+#include <iostream>
+#include "tid_mananger.h"
+
+    TID_manager::TID_manager(const std::vector<Thread*>& q) : thread_list(q) {}
+
+    int TID_manager::min_id_finder(){
+        for(int i=0; i<MAX_THREAD_NUM; ++i){
+            if(thread_list[i]==nullptr){
+                    return i;
+                }
+        }
+        return -1;
+    }
+
+    int TID_manager::get_size(){
+        return thread_list.size();
+    }
